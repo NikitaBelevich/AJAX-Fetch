7199724abaa5afdce5f18d9db62d92cba92c69b1
Nikita Belevich <belevish_1998@mail.ru>
2020-09-11 09:05:21 +0300

    Task 1 complete
    Получение данных по логинам пользователей GitHub с использованием промисов и fetch

--- js/app.js
+++ js/app.js
@@ -1,19 +1,48 @@
 'use strict';
 
 
+// https://api.github.com/users/NikitaBelevich
+/*
+Создайте асинхронную функцию getUsers(names), которая получает на вход массив логинов пользователей GitHub, запрашивает у GitHub информацию о них и возвращает массив объектов-пользователей.
+
+На каждого пользователя должен приходиться один запрос fetch.
+Запросы не должны ожидать завершения друг друга. Надо, чтобы данные приходили как можно быстрее.
+Если какой-то запрос завершается ошибкой или оказалось, что данных о запрашиваемом пользователе нет, то функция должна возвращать null в массиве результатов.
+*/
+
+const githubLogins = [
+    'NikitaBelevich',
+    'aikhelis',
+    'adsurov',
+    'sdfsdf43589dfugnf,', // null
+    'AnDeVerin',
+    'andreikinder',
+    'andriiwp3',
+    'AndreySavchenkov',
+    'al;sdjfl;sdfjlsakdjflsakdjf', // null
+    'BazovaLida',
+];
+
 (async () => {
-    // Получаем сразу результат промиса, т.е объект Response
-    let response = await fetch('https://api.github.com/users/NikitaBelevich');
-    console.warn(response);
-    // Получаем тело ответа из этого объекта
-    let data = await response.json();
-    console.warn(data);
+    // Получили от функции промис, await дождался его и вернул результат
+    const users = await getUsers(githubLogins);
+    console.group('GitHub Users');
+    console.info(users);
+    console.groupEnd('GitHub Users');
 })();
 
-// fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits')
-//     .then(response => {
-//         return response.json();
-//     })
-//     .then(data => {
-//         console.warn(data);
-//     })
+async function getUsers(usersLogins) {
+    const usersObjects = usersLogins.map(async userName => {
+        let userURL = `https://api.github.com/users/${userName}`; // Ссылка на данные n`го юзера
+        let response = await fetch(userURL);
+        if (response.ok) { // Если успешный запрос, тогда получаем данные и кладём в массив
+            let data = await response.json();
+            return data;
+        } else { // Иначе кладём в массив null и бросаем ошибку
+            console.error(`Ошибка HTTP: ${response.status}, URL: ${userURL}`);
+            return null;
+        }
+    });
+    // Возвращаем промис с результатами промисов
+    return Promise.all(usersObjects);
+}
